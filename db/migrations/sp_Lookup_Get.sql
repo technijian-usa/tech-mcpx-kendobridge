6923bb892ad56bbe725e8da44aa2a2dcf532d305
technijianorx <123764844+technijianorx@users.noreply.github.com>
2025-09-30 12:00:44 -0700

    updates

--- db/migrations/sp_Lookup_Get.sql
+++ db/migrations/sp_Lookup_Get.sql
@@ -1,13 +1,22 @@
-IF OBJECT_ID(N'dbo.sp_Lookup_Get', N'P') IS NOT NULL
-  DROP PROCEDURE dbo.sp_Lookup_Get;
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
 GO
-CREATE PROCEDURE dbo.sp_Lookup_Get
+
+CREATE OR ALTER PROCEDURE dbo.sp_Lookup_Get
   @Type NVARCHAR(100),
   @Key  NVARCHAR(200)
 AS
 BEGIN
   SET NOCOUNT ON;
-  SELECT TOP (1) [Value] FROM dbo.[Lookup] WITH (NOLOCK)
-  WHERE [Type] = @Type AND [Key] = @Key;
+
+  /* Read-committed for correctness; change to WITH (NOLOCK) only if you truly want dirty reads */
+  SELECT TOP (1) [Value]
+  FROM dbo.[Lookup] WITH (READCOMMITTED)
+  WHERE [Type] = @Type
+    AND [Key]  = @Key;
 END
 GO
+
+/* Optional: grant execute to a role/service principal you use for the API identity
+-- GRANT EXECUTE ON dbo.sp_Lookup_Get TO [YourDbRoleOrUser];
+*/
